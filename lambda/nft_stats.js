a860f44075dd0d717c6ee0b55dfa23abc8af2b32
shift-coredotlab <shift@coredotlab.io>
2021-06-28 19:11:25 +0000

    link stat

---
+++ lambda/nft_stats.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var AWS = require('aws-sdk');
+const dbPool = require('../modules/util_rds_pool.js');
+const dbQuery = require('../resource/sql.json');
+const psHandler = require('../modules/util_ps.js');
+var Base64 = require("js-base64");
+
+exports.handler = async function(event) {
+    console.log('[nft_stats_daily_insert]')
+    console.log('[EVENT]', event);
+
+    const isMaintenance = await psHandler.getParameterStoreValue(process.env.PARAMETER_STORE_VALUE, 'batch', null);
+    console.log('isMaintenance', isMaintenance)
+    if (isMaintenance) {
+        const message = JSON.parse(Base64.decode(isMaintenance)).message
+        console.log('[Maintenance]', message)
+    }
+    else {
+        try {
+            const pool = await dbPool.getPool();
+            await pool.query(dbQuery.nft_stats_daily_insert.queryString);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+};
